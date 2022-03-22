00e10c67424dd3c3beefb7f51a7fc74c49120885
aarthy4f <56670845+aarthy4f@users.noreply.github.com>
2022-03-22 09:52:28 +0530

    Added reporting details

--- src/test/java/e2e/businessspecific/precondition.java
+++ src/test/java/e2e/businessspecific/precondition.java
@@ -1,11 +1,15 @@
 package e2e.businessspecific;
 
 
+import com.relevantcodes.extentreports.LogStatus;
 import e2e.pages.InsuranceLoginPage;
+import e2e.util.ReportUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+
+
 
 public class precondition {
     private WebDriver driver;
@@ -15,27 +19,43 @@ public class precondition {
         this.driver=driver;
 
     }
-    public void verifypreselection(){
-        WebElement elm= driver.findElement(By.xpath("//label[contains(@data-testid,'buyingCarquoting')]"));
-        InsuranceLoginPage insuranceLoginPage=new InsuranceLoginPage(driver);
+    public void verifypreselection(String insurancestatus) throws Exception {
+        try {
 
-        String selected=insuranceLoginPage.newcarradioInput();
-        if(selected.contains("true")){
-            Assert.assertTrue(true, "Preslected checkbox");
-        }else{
-            insuranceLoginPage.newcarselectradio();
+            WebElement elm = driver.findElement(By.xpath("//label[contains(@data-testid,'buyingCarquoting')]"));
+            InsuranceLoginPage insuranceLoginPage = new InsuranceLoginPage(driver);
 
-            Assert.assertTrue(false, "Preslected checkbox not checked");
+            if (insurancestatus.contains("Modify")) {
+                ReportUtil.logMessage("Insurance Selection", "Change of Issuer");
+                insuranceLoginPage.existingcarselectradio();
+                ReportUtil.addScreenShot("Change of Issuer selected");
+            } else {
+                String selected = insuranceLoginPage.newcarradioInput();
+                if (selected.contains("true")) {
+                    Assert.assertTrue(true, "Preslected checkbox");
+                } else {
+                    insuranceLoginPage.newcarselectradio();
+
+                    Assert.assertTrue(true, "Preslected checkbox not checked");
+
+                }
+                ReportUtil.logMessage("Insurance Selection", "Purchase or change of owner");
+                ReportUtil.addScreenShot("Purchase or change of owner selected");
+
+            }
+
+            String inceptiondate = insuranceLoginPage.getinceptionDate();
+            if (inceptiondate.length() != 0) {
+                Assert.assertTrue(true, "Date already Selected");
+            } else {
+                insuranceLoginPage.setinceptionDate();
+                Assert.assertTrue(true, "Date is now set");
+
+            }
+
+        } catch (Exception e) {
+            ReportUtil.logMessage(LogStatus.FAIL,"TestExecution Failed due to exception",e.getMessage());
 
         }
-        String inceptiondate=insuranceLoginPage.getinceptionDate();
-        if(inceptiondate.length()!=0){
-            Assert.assertTrue(true, "Date already Selected");
-        }else{
-            insuranceLoginPage.setinceptionDate();
-            Assert.assertTrue(true, "Date is now set");
-
-        }
-
     }
-{}}
+}
--- src/test/java/e2e/businessspecific/vehicleselection.java
+++ src/test/java/e2e/businessspecific/vehicleselection.java
@@ -1,5 +1,6 @@
 package e2e.businessspecific;
 import e2e.pages.VehicleSelectPage;
+import e2e.util.ReportUtil;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,14 +15,60 @@ public class vehicleselection {
     public vehicleselection(WebDriver driver) {
 
         this.driver=driver;
-        VehicleSelectPage vehicleSelectPage=new VehicleSelectPage(driver);
 
     }
 
+    /**
+     * This function is for handling the registration related details during teh insurance flow
+     * @param insurancestatus
+     */
+ public void registration(String insurancestatus){
+
+
+     VehicleSelectPage vehicleSelectPage=new VehicleSelectPage(driver);
+     vehicleSelectPage.enteryearregiestered("02.2005");
+        if(insurancestatus.contains("Modify")) {
+            vehicleSelectPage.enterownerregiestered("02.2010");
+        }
+//        vehicleSelectPage.enterownerregiestered("02.2010");
+     vehicleSelectPage.clicksubmit();
+     vehicleSelectPage.enterbirthdate("11.11.1995");
+     ReportUtil.logMessage("Entering the Registration Details","Registration details entered successfully");
+
+ }
+
+    /**
+     * This function is to select the car to be insured by providing the HSN/TSN details
+     * @param hsn
+     */
+ public void selecthsn(String hsn){
+     ReportUtil.logMessage("Find via the HSN/TNS Finder","HSN Details:"+hsn);
+
+     VehicleSelectPage vehicleSelectPage=new VehicleSelectPage(driver);
+     String[] cardetails=hsn.split(":");
+     vehicleSelectPage.clickhsn();
+
+     vehicleSelectPage.enterCaryear(cardetails[0]);
+     vehicleSelectPage.enterCarhsn(cardetails[1]);
+     vehicleSelectPage.enterCartsn(cardetails[2]);
+     vehicleSelectPage.clicksubmit();
+     driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
+     vehicleSelectPage.clicksubmit();
+     vehicleSelectPage.enterbirthdate("11.11.1995");
 
 
 
+ }
+
+    /**
+     * This function is used the select the car based on the brand,model details
+     * @param car
+     * @param brand
+     * @param type
+     */
     public void selectmodel(String car,String brand,String type){
+        ReportUtil.logMessage("Find by make and model combination","car Details:"+car+" brand"+brand);
+
         VehicleSelectPage vehicleSelectPage=new VehicleSelectPage(driver);
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@name='list']")));
@@ -31,7 +78,7 @@ public class vehicleselection {
        // wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='makeFilter']")));
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        vehicleSelectPage.entermodel(car);
+        vehicleSelectPage.enterbrand(car);
         click(car);
 
         List<WebElement> listelm=driver.findElements(By.xpath(returnxpath(brand)));
@@ -43,18 +90,17 @@ public class vehicleselection {
      if(!type.contains("null")){
       click(type);
      }
-
-
         String fuel="Diesel";
         click(fuel);
-
-
         vehicleSelectPage.clickenginepower();
         vehicleSelectPage.clickengine();
 
-
-
     }
+
+    /**
+     * The Xpath is created dynamically using the value parameteer and the click operation performed
+     * @param value
+     */
     public void click(String value){
         String xpathvalue="//label[text()='"+value+"']/parent::div";
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
@@ -65,6 +111,12 @@ public class vehicleselection {
         elm.click();
 
     }
+
+    /**
+     * Create the dynamic xpath.
+     * @param value
+     * @return String
+     */
     public String returnxpath(String value){
         String xpathvalue="//label[text()='"+value+"']/parent::div";
     return xpathvalue;
