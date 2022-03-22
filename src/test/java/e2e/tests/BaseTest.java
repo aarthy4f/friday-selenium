b6a3c397c551fc2ee33349660f1b777bbd1a57ae
aarthy4f <56670845+aarthy4f@users.noreply.github.com>
2022-03-22 09:54:27 +0530

    Updated the test with reporting

--- src/test/java/e2e/tests/BaseTest.java
+++ src/test/java/e2e/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package e2e.tests;
 
 import java.time.Duration;
+
 import e2e.util.ExcelUtil;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestContext;
+
 import org.testng.annotations.*;
 
 import e2e.context.WebDriverContext;
@@ -23,121 +25,127 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  *
  * @author Aarthy
  */
-@Listeners({ ReportListener.class, LogListener.class })
+@Listeners({ReportListener.class, LogListener.class})
 public class BaseTest {
 
-	/** The driver. */
-	protected WebDriver driver;
-
-	/**
-	 * Global setup.
-	 */
-	@BeforeSuite(alwaysRun = true)
-	public void globalSetup() {
-		LoggerUtil.log("************************** Test Execution Started ************************************");
-		TestProperties.loadAllPropertie();
-	}
-
-	/**
-	 * Wrap all up.
-	 *
-	 * @param context the context
-	 */
-	@AfterSuite(alwaysRun = true)
-	public void wrapAllUp(ITestContext context) {
-		int total = context.getAllTestMethods().length;
-		int passed = context.getPassedTests().size();
-		int failed = context.getFailedTests().size();
-		int skipped = context.getSkippedTests().size();
-		LoggerUtil.log("Total number of testcases : " + total);
-		LoggerUtil.log("Number of testcases Passed : " + passed);
-		LoggerUtil.log("Number of testcases Failed : " + failed);
-		LoggerUtil.log("Number of testcases Skipped  : " + skipped);
-		boolean mailSent = MailUtil.sendMail(total, passed, failed, skipped);
-		LoggerUtil.log("Mail sent : " + mailSent);
-		LoggerUtil.log("************************** Test Execution Finished ************************************");
-	}
-
-	/**
-	 * Setup.
-	 */
-	@Parameters({"browser"})
-	@BeforeClass
-	protected void setup(String browser) throws Exception {
- 	//String browser="chrome";
-//		System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
-		if(browser.equalsIgnoreCase("firefox")){
-			//create firefox instance
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-		}
-		//Check if parameter passed as 'chrome'
-		else if(browser.equalsIgnoreCase("chrome")){
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions options = new ChromeOptions();
-			// options = new ChromeOptions();
-			options.addArguments("enable-automation");
-			//options.addArguments("--headless");
-			options.addArguments("--window-size=1920,1080");
-			options.addArguments("--no-sandbox");
-			options.addArguments("--disable-extensions");
-			options.addArguments("--dns-prefetch-disable");
-			options.addArguments("--disable-gpu");
-			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-			//ops.addArguments("--lang=en");
-
-			driver = new ChromeDriver(options);
+    /**
+     * The driver.
+     */
+    public WebDriver driver;
 
 
-		}
-		//Check if parameter passed as 'Edge'
-		else if(browser.equalsIgnoreCase("Edge")){
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-		}
-		else{
-			//If no browser passed throw exception
-			throw new Exception("Browser is not correct");
-		}
+    /**
+     * Global setup.
+     */
+    @BeforeSuite(alwaysRun = true)
+    public void globalSetup() {
+        LoggerUtil.log("************************** Test Execution Started ************************************");
+        TestProperties.loadAllPropertie();
+    }
+
+    /**
+     * Wrap all up.
+     *
+     * @param context the context
+     */
+    @AfterSuite(alwaysRun = true)
+    public void wrapAllUp(ITestContext context) {
+        int total = context.getAllTestMethods().length;
+        int passed = context.getPassedTests().size();
+        int failed = context.getFailedTests().size();
+        int skipped = context.getSkippedTests().size();
+        LoggerUtil.log("Total number of testcases : " + total);
+        LoggerUtil.log("Number of testcases Passed : " + passed);
+        LoggerUtil.log("Number of testcases Failed : " + failed);
+        LoggerUtil.log("Number of testcases Skipped  : " + skipped);
+        boolean mailSent = MailUtil.sendMail(total, passed, failed, skipped);
+        LoggerUtil.log("Mail sent : " + mailSent);
+        LoggerUtil.log("************************** Test Execution Finished ************************************");
+    }
+
+    /**
+     * Setup.
+     */
+    @Parameters({"browser"})
+    @BeforeClass
+    public void setup(String browser) throws Exception {
+
+        if (browser.equalsIgnoreCase("firefox")) {
+            //create firefox instance
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        //Check if parameter passed as 'chrome'
+        else if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            // options = new ChromeOptions();
+            options.addArguments("enable-automation");
+            //options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--dns-prefetch-disable");
+            options.addArguments("--disable-gpu");
+            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+            //ops.addArguments("--lang=en");
+
+            driver = new ChromeDriver();
 
 
-		WebDriverContext.setDriver(driver);
-		driver.get("https://hello.friday.de/quote/selectPrecondition");
-		driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(4));
-		WebElement root1 = driver.findElement(By.id("usercentrics-root"));
-		JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
-		SearchContext shadowRoot1 = (SearchContext) javascriptExecutor.executeScript("return arguments[0].shadowRoot", root1);
-		shadowRoot1.findElement(By.cssSelector("button[data-testid=uc-accept-all-button]")).click();
+        }
+        //Check if parameter passed as 'Edge'
+        else if (browser.equalsIgnoreCase("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            //If no browser passed throw exception
+            throw new Exception("Browser is not correct");
+        }
+
+        //WebDriverContext WebDriverContext= new WebDriverContext();
+        WebDriverContext.setDriver(driver);
+        driver.get("https://hello.friday.de/quote/selectPrecondition");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(4));
+        WebElement root1 = driver.findElement(By.id("usercentrics-root"));
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        SearchContext shadowRoot1 = (SearchContext) javascriptExecutor.executeScript("return arguments[0].shadowRoot", root1);
+        shadowRoot1.findElement(By.cssSelector("button[data-testid=uc-accept-all-button]")).click();
+
+
+    }
+
+    @BeforeMethod
+    public void launchapplication() {
+        driver.get("https://hello.friday.de/quote/selectPrecondition");
+
+    }
+
+    @DataProvider(name = "cardetails", parallel = false)
+
+    public static Object[][] cardetails() throws Exception {
+
+        // The number of times data is repeated, test will be executed the same no. of times
+
+        Object[][] testObjArray = ExcelUtil.getTableArray();
+
+
+        return (testObjArray);
+
+
+    }
 
 
 
-	}
-	 @BeforeMethod
-	 public void launchapplication(){
-		 driver.get("https://hello.friday.de/quote/selectPrecondition");
+    /**
+     * Wrap up.
+     */
+    @AfterClass
+    public void wrapUp() {
 
-	 }
-	@DataProvider(name = "cardetails")
-
-	public static Object[][] cardetails() throws Exception{
-
-	 // The number of times data is repeated, test will be executed the same no. of times
-
-	 Object[][] testObjArray = ExcelUtil.getTableArray();
-
-	 return (testObjArray);
-
-
-
-	}
-	/**
-	 * Wrap up.
-	 */
-	@AfterClass
-	public void wrapUp() {
-		if (driver != null) {
-			driver.close();
-			driver.quit();
-		}
-	}
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+    }
 }
--- src/test/java/e2e/tests/InsuranceTest.java
+++ src/test/java/e2e/tests/InsuranceTest.java
@@ -1,11 +1,21 @@
 package e2e.tests;
 
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import e2e.businessspecific.precondition;
 import e2e.businessspecific.vehicleselection;
+import e2e.context.WebDriverContext;
 import e2e.pages.InsuranceLoginPage;
 
+import e2e.pages.VehicleSelectPage;
+import e2e.report.ExtentReportManager;
+import e2e.util.ReportUtil;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -18,29 +28,44 @@ import java.time.Duration;
 public class InsuranceTest extends BaseTest {
     static ExtentTest test;
 
-    @Test(dataProvider = "cardetails",testName = "Purchase insurance for car")
-    public void InsuranceTest(String car, String model, String type) {
+
+
+    @Test(dataProvider = "cardetails", testName = "Purchase insurance for car")
+    public void InsuranceTest(String insurancestatus, String car, String model, String type, String hsn) throws Exception {
+        ReportUtil.logMessage("Test case Details", "Adding the insurance for the car:" + car + " model:+" + model);
+
+        ReportUtil.addScreenShot(LogStatus.INFO, "Default selection");
         System.out.println(car);
         System.out.println(model);
         System.out.println(type);
+        System.out.println(insurancestatus);
+        System.out.println(hsn);
 
-        InsuranceLoginPage insuranceLoginPage=new InsuranceLoginPage(driver);
-        precondition precondition=new precondition(driver);
-        precondition.verifypreselection();
+
+        InsuranceLoginPage insuranceLoginPage = new InsuranceLoginPage(driver);
+        precondition precondition = new precondition(driver);
+        precondition.verifypreselection(insurancestatus);
         insuranceLoginPage.clicksubmit();
 
-       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-       wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
-       insuranceLoginPage.clicksubmit();
-       vehicleselection vehicle=new vehicleselection(driver);
-       vehicle.selectmodel(car,model,type);
-       WebElement elm=driver.findElement(By.xpath(("//input[@name='monthYearFirstRegistered']")));
-       elm.sendKeys("11.1989");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+        insuranceLoginPage.clicksubmit();
+        VehicleSelectPage vehicleobj = new VehicleSelectPage(driver);
+        vehicleselection vehicle = new vehicleselection(driver);
+        if (hsn.contains("null")) {
+
+            vehicle.selectmodel(car, model, type);
+
+            vehicle.registration(insurancestatus);
+        } else {
+
+            vehicle.selecthsn(hsn);
+        }
+        String date = vehicleobj.getbirthdate();
 
 
 
-
-       Assert.assertTrue(elm.getText()!=null, "Test Execution success");
+        Assert.assertTrue(date != null, "Test Execution success");
     }
 
 
